c6c180840e8e46c05e9b98847497c0e4c3f9e6c4
Kenna Neitch <kdneitch@gmail.com>
2019-06-04 15:15:01 -0500

    four places

---
+++ python/ExercisePlaces.py
@@ -0,0 +1,6 @@
+places = ["Belize", "Costa Rica", "El Salvador", "Oregon"]
+results = []
+for place in places:
+    results = ["I would like to visit " + place for place in places]
+
+print(results)
