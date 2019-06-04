1321b9b86e7019a4c15a3e4fac1cb3ad641b9117
Kenna Neitch <kdneitch@gmail.com>
2019-06-04 13:28:00 -0500

    my info in Exercise 1 file

---
+++ python/CodeExercise1.py
@@ -0,0 +1,8 @@
+address_info = {
+"name": "Kenna Neitch",
+"street": "3018 48th Street",
+"city": "Lubbock", "state": "Texas", "zip": "79413",}
+
+print(address_info["name"])
+print(address_info["street"])
+print(address_info["city"] + ", " + address_info["state"] + " " + address_info["zip"])
