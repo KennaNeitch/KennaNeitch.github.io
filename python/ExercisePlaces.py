bc7d163f8df6dc499e3802f87c65a7901f47a354
Kenna Neitch <kdneitch@gmail.com>
2019-06-04 15:17:41 -0500

    places code revised

--- python/ExercisePlaces.py
+++ python/ExercisePlaces.py
@@ -1,6 +1,10 @@
 places = ["Belize", "Costa Rica", "El Salvador", "Oregon"]
-results = []
-for place in places:
-    results = ["I would like to visit " + place for place in places]
+# results = []
+# for place in places:
+#     results = ["I would like to visit " + place for place in places]
+#
+# print(results)
 
-print(results)
+
+for place in places:
+    print("I would like to visit " + place + ".")
