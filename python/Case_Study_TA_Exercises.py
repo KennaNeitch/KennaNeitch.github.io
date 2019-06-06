600d67b1652ca3db85a22cca62fc93bcf22af16a
Kenna Neitch <kdneitch@gmail.com>
2019-06-06 14:19:42 -0500

    Case Study Textual Analysis Exercise 2

---
+++ python/Case_Study_TA_Exercises.py
@@ -0,0 +1,125 @@
+from bs4 import BeautifulSoup
+from urllib import request
+
+# url = "https://raw.githubusercontent.com/humanitiesprogramming/scraping-corpus/master/full-text.txt"
+# html = request.urlopen(url).read()
+# soup = BeautifulSoup(html, "lxml")
+# raw_text = soup.text
+# texts = eval(soup.text)
+
+# print(len(raw_text))
+# print(len(texts))
+
+import nltk
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+
+# tokenized_texts = []
+# for text in texts:
+#     tokenized_texts.append(word_tokenize(text))
+#
+# for tokenized_text in tokenized_texts:
+#     # print("=====")
+#     # print(len(tokenized_text))
+#     # print(tokenized_text[0:20])
+#
+#     doyle = tokenized_texts[:5]
+#     bronte = tokenized_texts[5:]
+#
+# # print(len(doyle))
+# # print(len(bronte))
+#
+# def normalize(tokens):
+#     """Takes a list of tokens and returns a list of tokens that has been normalized by lowercasing all tokens and removing Project Gutenberg frontmatter."""
+#
+#     #lowercase all words
+#     normalized = [token.lower() for token in tokens]
+#
+#     #very rough end of front matter.
+#     end_of_front_matter = 90
+#     #very rough beginning of end matter.
+#     start_of_end_matter = -2973
+#     #get only the text between the end matter and front end_of_front_matter
+#     normalized = normalized[end_of_front_matter:start_of_end_matter]
+#
+#     return normalized
+# # print(normalize(bronte[0])[:200])
+# # print(normalize(bronte[0])[-200:])
+# doyle = [normalize(text) for text in doyle]
+# bronte = [normalize(text) for text in bronte]
+#
+# # print(doyle[0][:30])
+# from nltk.corpus import stopwords
+# # print(stopwords.words("english")[0:30])
+# def remove_stopwords(tokens):
+#     return [token for token in tokens if token not in stopwords.words("english")]
+# # print(len(doyle[0]))
+# # print("start cleaning")
+# # doyle = [remove_stopwords(text) for text in doyle]
+# # print("doyle done")
+# # bronte = [remove_stopwords(text) for text in bronte]
+# # print("bronte done")
+# #
+# # print(len(doyle[0]))
+# example = nltk.FreqDist(doyle[0])
+# # print(example.most_common(20))
+# doyle_freq_dist = [nltk.FreqDist(text) for text in doyle]
+# bronte_freq_dist = [nltk.FreqDist(text) for text in bronte]
+#
+# def print_top_words(freq_dist_text):
+#     """Takes a frequency distribution of a text and prints out the top 10 words in it."""
+#     print("=====")
+#     print(freq_dist_text.most_common(10))
+#     print("=====")
+#
+# # for text in doyle_freq_dist:
+# #     print_top_words(text)
+# # for text in bronte_freq_dist:
+# #     print_top_words(text)
+# # print(doyle_freq_dist[0]["holmes"])
+# # print(bronte_freq_dist[0]["would"])
+# def get_counts_in_corpora(token, corpus_one, corpus_two):
+#     """Take two corpora, represented as lists of frequency distributions, and token query. Return the frequency of that token in all the texts in the corpus. The result should be a list of two lists, one for each text."""
+#     corpus_one_counts = [text_freq_dist[token] for text_freq_dist in corpus_one]
+#     corpus_two_counts = [text_freq_dist[token] for text_freq_dist in corpus_two]
+#     return [corpus_one_counts, corpus_two_counts]
+
+class Text:
+    def __init__(self, raw_tokens):
+        print("making a test")
+        self.raw_tokens = raw_tokens
+        self.raw_tokens = word_tokenize(self.raw_tokens)
+        self.clean_tokens = self.normalize(self.raw_tokens)
+        self.clean_tokens = self.remove_stopwords(self.clean_tokens)
+        self.fq = nltk.FreqDist(self.clean_tokens)
+
+    def normalize(self, tokens):
+        """Takes a list of tokens and returns a list of tokens that has been normalized by lowercasing all tokens and removing Project Gutenberg frontmatter."""
+
+        #lowercase all words
+        normalized = [token.lower() for token in tokens]
+
+        #very rough end of front matter.
+        end_of_front_matter = 90
+        #very rough beginning of end matter.
+        start_of_end_matter = -2973
+        # get only the text between the end matter and front matter
+        normalized = normalized[end_of_front_matter:start_of_end_matter]
+        return normalized
+    def remove_stopwords(self, tokens):
+        #remove all the stopwords from a list of raw_tokens
+        return [token for token in tokens if token not in stopwords.words("english")]
+
+        #do the thing
+        #get the texts from the github repo
+        # scraping corpus
+url = "https://raw.githubusercontent.com/humanitiesprogramming/scraping-corpus/master/full-text.txt"
+html = request.urlopen(url).read()
+soup = BeautifulSoup(html, "lxml")
+raw_text = soup.text
+texts = eval(soup.text)
+
+doyle = [Text(raw_text) for raw_text in texts[:2]]
+bronte = [Text(raw_text) for raw_text in texts[:8]]
+
+print(doyle[0].fq.most_common(50))
--- python/Case_Study_Textual_Analysis.py
+++ python/Case_Study_Textual_Analysis.py
@@ -95,6 +95,6 @@ def get_counts_in_corpora(token, corpus_one, corpus_two):
 # print(corpus_one_results)
 # print(corpus_two_results)
 
-from matplotlib import pylab
-nltk.Text(doyle[0]).dispersion_plot(["evidence", "clue", "science", "love", "say", "said"])
-nltk.Text(bronte[0]).dispersion_plot(["evidence", "clue", "science", "say", "said"])
+# from matplotlib import pylab
+# nltk.Text(doyle[0]).dispersion_plot(["evidence", "clue", "science", "love", "say", "said"])
+# nltk.Text(bronte[0]).dispersion_plot(["evidence", "clue", "science", "say", "said"])
