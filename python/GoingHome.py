68ed95c9b4ae2c689a509b532c23b7a977ec86dc
Kenna Neitch <kdneitch@gmail.com>
2019-06-06 19:02:07 -0500

    message for getting home

---
+++ python/GoingHome.py
@@ -0,0 +1,5 @@
+qualities_I_love = ["is the goofiest goober.", "has a pretty cutie booty.", "thinks he has my goat.", "is the most bestest boo,"]
+for quality in qualities_I_love:
+    print("Timothy " + quality)
+
+print(" and I love him ALL the bunches!")
